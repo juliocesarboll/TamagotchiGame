70454bc6bd19a93d7af619ba7e29aea069e40a1b
Julio Boll <juliocesarboll@gmail.com>
2017-01-10 11:12:07 -0200

    Total Rework

--- src/game/tamagotchi/Tamagotchi.java
+++ src/game/tamagotchi/Tamagotchi.java
@@ -3,53 +3,78 @@ package game.tamagotchi;
 public class Tamagotchi {
 	private String name;
 	private int age;
+	private String feeling;
 	private double weight;
-	private int wish;
+	private String lastWish;
+	private String nextWish;
+	private double knowledge;
+	private int dirty;
+	private double muscle;
 	
 	public Tamagotchi(String name){
-		this.name = name;
-		this.age = 1;
-		this.weight = 1.0;
-		this.wish = 0;
+		setName(name);
+		setAge(1);
+		setWeight(0.5);
+//		setFeeling();
+//		setLastWish();
+		setKnowledge(0.8);
+		setDirty(1);
+		setMuscle(0.3);
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public double getWeight() {
 		return weight;
 	}
-
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
-
-	public int getWish() {
-		return wish;
+	public String getFeeling() {
+		return feeling;
 	}
-
-	public void setWish(int wish) {
-		this.wish = wish;
+	public void setFeeling(String feeling) {
+		this.feeling = feeling;
 	}
-
-	@Override
-	public String toString() {
-		return "Tamagotchi [name=" + name + ", age=" + age + ", weight=" + weight + ", wish=" + wish + "]";
+	public String getLastWish() {
+		return lastWish;
+	}
+	public void setLastWish(String lastWish) {
+		this.lastWish = lastWish;
+	}
+	public String getNextWish() {
+		return nextWish;
+	}
+	public void setNextWish(String nextWish) {
+		this.nextWish = nextWish;
+	}
+	public double getKnowledge() {
+		return knowledge;
+	}
+	public void setKnowledge(double knowledge) {
+		this.knowledge = knowledge;
+	}
+	public int getDirty() {
+		return dirty;
+	}
+	public void setDirty(int dirty) {
+		this.dirty = dirty;
+	}
+	public double getMuscle() {
+		return muscle;
+	}
+	public void setMuscle(double muscle) {
+		this.muscle = muscle;
 	}
 	
-	
-		
 }
